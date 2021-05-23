bb4588d1c37a6b95f3c300ed19b65c728b7894f2
Ios Kateyka <ioskateyka@gmail.com>
2021-05-23 12:57:36 +0300

    1.Added view that changes color 2.Added two labels and one slider to control the colors of the view 3. Implemented the logic for calculating the color depending on the dragging of the slider 4. The color that the user sets through the slider is displayed in labels 5. The color that the user sets is passed to the view

--- RepaintingViewWithSliders/ViewController.swift
+++ RepaintingViewWithSliders/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var repaintingView: UIView!
+    
+    @IBOutlet weak var redColorCountLabel: UILabel!
+    @IBOutlet weak var greenColorCountLabel: UILabel!
+    @IBOutlet weak var blueColorCountLabel: UILabel!
+    
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
+    //    MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startValueForSliders()
+        
     }
-
-
+    override func viewWillLayoutSubviews() {
+        repaintingView.layer.cornerRadius = 10
+    }
+    func startValueForSliders() {
+        redColorSlider.value = 0
+        redColorSlider.minimumTrackTintColor = .red
+        greenColorSlider.value = 0
+        greenColorSlider.minimumTrackTintColor = .green
+        blueColorSlider.value = 0
+        blueColorSlider.minimumTrackTintColor = .blue
+    }
+    func calculationColor() ->UIColor {
+        
+        let red : CGFloat
+        let green : CGFloat
+        let blue : CGFloat
+        let colorCount: Float = 255.0
+        let percent : Float = 100.0
+        
+        red = CGFloat(redColorSlider.value * percent / colorCount  )
+        green = CGFloat(greenColorSlider.value * percent / colorCount )
+        blue = CGFloat(blueColorSlider.value * percent / colorCount)
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 0.8)
+        
+        return color
+    }
+    func assignmentColor() {
+        let color = calculationColor()
+        repaintingView.backgroundColor = color
+    }
+    func assigmentColorCountToLabel() {
+        let red  = redColorSlider.value
+        let green = greenColorSlider.value
+        let blue = blueColorSlider.value
+        
+        //        преобразование в три символа для лейбла с подсчетом цвета
+        redColorCountLabel.text = String(format: "%.2f", red)
+        greenColorCountLabel.text = String(format: "%.2f", green)
+        blueColorCountLabel.text = String(format: "%.2f", blue)
+    }
+    @IBAction func dragSliders(_ sender: Any) {
+        assignmentColor()
+        assigmentColorCountToLabel()
+    }
+    
+    
 }
 
